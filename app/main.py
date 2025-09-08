7f66f4f87375ae0534ce82e8f459b757ce77101d
Livissima <livianevesgeo@gmail.com>
2025-09-08 18:03:24 -0300

    Primeiras tentativas de organização de threading.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 import concurrent.futures
+import threading
+
 from app.ui.screens.janela import Janela
 
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    thread_janela = executor.submit(Janela)
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     thread_janela = executor.submit(Janela)
 
 
+# thread_janela = threading.Thread(
+#     target=Janela,
+#     daemon=False
+# )
+# thread_janela.start()
 
+Janela()
