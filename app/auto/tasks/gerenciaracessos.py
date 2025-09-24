44a15ba4d5b89a044bb9e8724d6c3ef0f871079b
Livissima <livianevesgeo@gmail.com>
2025-09-24 17:57:26 -0300

    Reformando o gerenciador de acessor para que ele consiga lidar com emails educacionais

--- app/auto/tasks/gerenciaracessos.py
+++ app/auto/tasks/gerenciaracessos.py
@@ -1,11 +1,13 @@
 import sys
 from pandas import DataFrame
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
 from app.auto.functions.navegação import Navegação
 from app.auto.data.sites.propriedades import Propriedades
 from typing import Literal
 import pandas as pd
-
+import unicodedata
 
 class GerenciarAcessos:
 
@@ -18,39 +20,42 @@ class GerenciarAcessos:
             **kwargs
     ):
         print(f'class NetEscola instanciada.')
+
         self.master = navegador
         self.nv = Navegação(navegador, tipo)
         self.pp = Propriedades(site=tipo)
-        self.path_db = path_database
         self.turmas = turmas
+        print(f'{self.turmas = }')
 
-        self.df = self.dataframe()
+        self.df = self.dataframe(path_database)
         self.gerenciar(tipo)
-        self.master.quit()
+        # self.master.quit()
 
 
-    def dataframe(self):
-        _df = pd.read_excel(self.path_db, 'Base Ativa')
-        df: DataFrame = _df[
-            ['Turma', 'Matrícula', 'Educacional', 'Estudante', 'Data de Nascimento', 'Senha padrão', 'Nova senha']
-        ].copy()
+    def dataframe(self, path):
+        _df = pd.read_excel(path, 'Base Ativa')
+        colunas = ['Turma', 'Matrícula', 'Educacional', 'Estudante', 'Data de Nascimento', 'Senha padrão', 'Nova senha']
+        df: DataFrame = _df[colunas].copy()
+
+        df['Turma'] = df['Turma'].astype(str).str.strip()
+
+        df['Estudante'] = df['Estudante'].astype(str).str.normalize('NFKD').str.encode('ASCII', errors='ignore').str.decode('ASCII').str.strip()
+
+        print(f'{df['Turma'].unique().tolist() = }')
+
         df = df[df['Turma'].isin(self.turmas)]
-        print(f'turmas {df['Turma'].unique().tolist()}')
+
         return df
 
-    def gerenciar(self, plataforma):
+    def gerenciar(self, tipo):
         self.master.get(self.pp.url)
-        plataformas = {
-            'netescola' : self.gerenciar_netescola,
-            'google'    : self.gerenciar_google,
-        }
-        return plataformas[plataforma]()
 
-    def gerenciar_netescola(self):
         def anunciar(ind, aluno: str, _turma: str, texto: str):
             sys.stdout.write(f'\r {ind} {aluno} - {_turma}: {texto}.')
             sys.stdout.flush()
         início = 0
+        if tipo not in ('netescola', 'google') :
+            raise Exception(f'Tipo desconhecido: {tipo}')
 
         for índice, linha in self.df.iloc[início:].iterrows():
             estudante  = str(linha['Estudante'])
@@ -59,26 +64,50 @@ class GerenciarAcessos:
             email      = str(linha['Educacional'])
             dn         = str(linha['Data de Nascimento']).replace('/', '')
             nova_senha = str(linha['Nova senha'])
+            senha_padrão = str(linha['Senha padrão'])
+            senha_padrão2 = str(f'go2025{linha['Estudante'].lower().split()[0]}')
 
-            anunciar(índice, estudante, turma, 'iniciando')
+            print(estudante, turma, matrícula, email, dn, nova_senha, senha_padrão)
 
-            self.nv.digitar_xpath('matrícula', string=matrícula)
-            self.nv.digitar_xpath('nascimento', string=dn)
-            self.nv.clicar('xpath', 'pesquisar')
+            # anunciar(índice, estudante, turma, 'iniciando')
+            #
+            # if tipo == 'netescola':
+            #     self.gerenciar_netescola(matrícula, dn, email, nova_senha)
+            #
+            #     anunciar(índice, estudante, turma, 'concluído.')
+            #     print('\r')
+            #     self.master.refresh()
+            #     self.nv.aguardar_página()
+            #
+            if tipo == 'google':
+                self.gerenciar_google(estudante, email, senha_padrão, senha_padrão2, dn)
 
-            self.nv.digitar_xpath('email', string=email)
-            self.nv.digitar_xpath('email2', string=email)
-            self.nv.digitar_xpath('senha', string=nova_senha)
-            self.nv.digitar_xpath('senha2', string=nova_senha)
-            self.nv.clicar('xpath', 'concordo')
-            self.nv.clicar('xpath', 'salvar')
-            self.nv.aguardar_página()
-            anunciar(índice, estudante, turma, 'concluído.')
-            print('\r')
-            self.master.refresh()
-            self.nv.aguardar_página()
 
-    @staticmethod
-    def gerenciar_google():
-        print('GOOGLE')
+    def gerenciar_netescola(self, matrícula, dn, email, nova_senha):
+        self.nv.digitar_xpath('matrícula', string=matrícula)
+        self.nv.digitar_xpath('nascimento', string=dn)
+        self.nv.clicar('xpath', 'pesquisar')
+
+        self.nv.digitar_xpath('email', string=email)
+        self.nv.digitar_xpath('email2', string=email)
+        self.nv.digitar_xpath('senha', string=nova_senha)
+        self.nv.digitar_xpath('senha2', string=nova_senha)
+        self.nv.clicar('xpath', 'concordo')
+        self.nv.clicar('xpath', 'salvar')
+        self.nv.aguardar_página()
+
+    def gerenciar_google(self, estudante, email, senha_padrão, senha_padrão2, dn):
+        self.nv.digitar_xpath('input email', string=email),
+        self.nv.clicar('xpath', 'avançar email')
+
+        self.nv.digitar_xpath('input senha', string='senha_padrão')
+        self.nv.clicar('xpath', 'avançar senha')
+
+        if self.master.find_element(By.XPATH, '//*[@id="c0"]'):
+            print('errou')
+            self.nv.digitar_xpath('input senha', string='senha_padrão2')
+
+            # self.nv.digitar_xpath('input senha', string=dn)
+            # self.nv.clicar('xpath', 'avançar senha')
+
         pass
