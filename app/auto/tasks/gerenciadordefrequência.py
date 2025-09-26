800eca8e7030fa9802571fef9cbfba265c22d6ef
Livissima <128195954+Livissima@users.noreply.github.com>
2025-09-26 01:58:49 -0300

    Gerenciador de Frequência agora executa javascript para interagir com as divs de falta e selects de justificativa.

--- app/auto/tasks/gerenciadordefrequência.py
+++ app/auto/tasks/gerenciadordefrequência.py
@@ -1,92 +1,124 @@
 import pandas as pd
-from pandas import DataFrame, Series
 from selenium.webdriver.common.by import By
 from app.auto.data.sites.propriedades import Propriedades
 from app.auto.functions.navegação import Navegação
 from selenium.webdriver import Chrome
 
-class GerenciadorDeFrequência:
 
-    def __init__(self, navegador: Chrome, path, **kwargs):
+class GerenciadorDeFrequência :
+
+    def __init__(self, navegador: Chrome, path, **kwargs) :
         self.path_xlsx = path
         self.master = navegador
         self.nv = Navegação(navegador, 'siap')
         self.pp = Propriedades(site='siap')
+        print(f'{self.ausentes_do_dia = }')
         self.executar()
-        # self.master.quit()
+        self.master.quit()
 
-    def executar(self):
+    def executar(self) :
         self.master.get(self.pp.url)
         self.master.maximize_window()
         self._logon()
-        self._ir_painel_frequência()
-        self._presenciar_todos()
+        self._acessar_painel_frequência()
+        self._iterar_turmas()
 
-    def _logon(self):
+    def _logon(self) :
         credenciais = self.pp.credenciais
         self.nv.digitar_xpath('input login', string=credenciais['id'])
         self.nv.digitar_xpath('input senha', string=credenciais['senha'])
-        self._passar_captcha()
+        self._resolver_captcha()
         self.nv.clicar('xpath', 'botão login')
         self.nv.aguardar_página()
 
-    def _passar_captcha(self):
+    def _resolver_captcha(self) :
         captcha = self.master.find_element(By.XPATH, self.pp.xpaths['captcha'])
         self.nv.digitar_xpath('input captcha', string=captcha.text)
 
-    def _ir_painel_frequência(self):
+    def _acessar_painel_frequência(self) :
         self.nv.clicar('xpath', 'menu sistema')
         self.nv.clicar('xpath', 'menu frequência')
         self.nv.aguardar_página()
 
-    def _presenciar_todos(self):
+    def _iterar_turmas(self) :
         turmas = self.nv.obter_turmas_siap()
-
-        for turma in turmas:
+        for turma in turmas :
             self.nv.clicar('xpath livre', turma)
+            nome_turma = self.master.find_element(By.XPATH, turma).text
             self.nv.aguardar_página()
+            print('página carregada')
+            self._definir_faltas(nome_turma)
 
+    def _definir_faltas(self, turma):
+        matrículas_clicadas = self._executar_javascript('falta')
+        self._executar_javascript('justificativa')
+        for matrícula in matrículas_clicadas:
+            estudante = self.ausentes_do_dia.get(matrícula, 'Não identificado')
+            print(f'Falta lançada para: {estudante} - {matrícula}')
+        print(f'Faltas lançadas na turma {turma}: {len(matrículas_clicadas)}')
 
-            elemento_coluna_pontinhos = self.master.find_element(By.CLASS_NAME, 'listaDeFrequencias')
-            sub_elemento_pontinhos = elemento_coluna_pontinhos.find_element(By.CLASS_NAME, 'itens')
-            lista_pontinhos = sub_elemento_pontinhos.find_elements(By.CSS_SELECTOR, 'div[data-matricula]')
-            alvos = [alvo.click() for alvo in lista_pontinhos if alvo in self.faltosos_de_hoje]
-
-            elemento_coluna_justificativas = self.master.find_element(By.CLASS_NAME, 'listaMotivoAusencia')
-            sub_elemento_justificativas = elemento_coluna_justificativas.find_element(By.CLASS_NAME, 'itens')
-            lista_divs = sub_elemento_justificativas.find_elements(By.CSS_SELECTOR, )
-            justis = [div.find_element(By.TAG_NAME, 'select') for div in lista_divs if div.get_attribute('data-matrícula') in self.faltosos_de_hoje['Matrícula'] ]
-            #Não testado
-
-
-
-
-
-
-            self.nv.clicar('xpath', 'salvar e próximo')
-
-
-
-
-
+        self.nv.clicar('xpath', 'salvar e próximo')
 
     @property
-    def faltosos_de_hoje(self) -> DataFrame:
-        df = pd.read_excel(self.path_xlsx, sheet_name='Compilado Faltas')
-
+    def ausentes_do_dia(self) -> dict :
+        df = pd.read_excel(self.path_xlsx, sheet_name='Compilado Faltas', dtype={'Matrícula' : str})
         df = df[['Turma Real', 'Estudante', 'Data Falta', 'Lançado', 'Matrícula']]
-
         df = df[df['Lançado'] == 'Lançado']
-
         df['Data Falta'] = df['Data Falta'].dt.strftime('%d/%m/%Y')
-
-        faltosos_de_hoje = df[df['Data Falta'] == self.pp.hoje]
-
-        # faltosos_de_hoje = list(df_hoje['Estudante'])
-
-        print(f'{type(faltosos_de_hoje) = }')
+        df = df[df['Data Falta'] == self.pp.hoje]
+        faltosos_de_hoje = dict(zip(df['Matrícula'], df['Estudante']))
 
         return faltosos_de_hoje
 
+    @property
+    def __script_js_faltas(self):
+        return """
+        const matriculasNecessarias = new Set(arguments[0]);
+        const elementos = document.querySelectorAll('.listaDeFrequencias .itens div[data-matricula]');
+        const matriculasClicadas = [];
 
+        elementos.forEach(elemento => {
+            const matricula = elemento.getAttribute('data-matricula');
+            if (matriculasNecessarias.has(matricula)) {
+                elemento.click();
+                matriculasClicadas.push(matricula);
+                }
+            });
+        return matriculasClicadas;
+        """
 
+    @property
+    def __script_js_justificativas(self):
+        return """
+            const matriculasNecessarias = new Set(arguments[0]);
+            const selects = document.querySelectorAll('.listaMotivoAusencia .itens select[data-matricula]');
+            const resultados = [];
+
+            selects.forEach(select => {
+                const matricula = select.getAttribute('data-matricula');
+                if (matriculasNecessarias.has(matricula)) {
+                    const valorAntigo = select.value;
+                    select.value = '1';
+                    const valorNovo = select.value;
+
+                    ['change', 'input', 'blur'].forEach(evento => {
+                        select.dispatchEvent(new Event(evento, { bubbles: true }));
+                    });
+                    resultados.push({
+                        matricula: matricula,
+                        alterado: valorNovo === '1',
+                        valorAntigo: valorAntigo,
+                        valorNovo: valorNovo
+                    });
+                }
+            });
+            return resultados;
+            """
+
+    def _executar_javascript(self, tipo):
+        scripts = {
+            'falta' : self.__script_js_faltas,
+            'justificativa' : self.__script_js_justificativas
+        }
+        argumento = list(self.ausentes_do_dia.keys())
+        return self.master.execute_script(scripts[tipo], argumento)
