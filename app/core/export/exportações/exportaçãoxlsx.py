447faed6d3c3c80d8ddc25035849b7a33866dcc5
Livissima <128195954+Livissima@users.noreply.github.com>
2025-10-21 00:31:47 -0300

    Aprimoramentos com base em OC e DDD. isntâncias de tipos Primitivos sendo encapsulados, classes gigantes sendo desmembradas, elses sendo eliminados, lógica de getters e setters sendo reformada, interfaces sendo elaboradas. Muitas classes seguem com alto acoplamento.

--- app/core/export/exportações/exportaçãoxlsx.py
+++ app/core/export/exportações/exportaçãoxlsx.py
@@ -1,76 +1,89 @@
 import os
-from typing import Literal
+from os import PathLike
+from pathlib import Path
+from typing import Literal, Any
 from pandas import DataFrame, ExcelWriter
 
-#todo: Adicionar método para definir diretório_fonte de output pela UI
+
 #todo: adicionar método para salvar os paths selecionados
 
 class ExportaçãoXLSX:
-    def __init__(self, consulta, path):
-        self.consulta = consulta
-        self.path = path
-        self._exportar_xlsx()
+    NOME_XLSX = 'Database'
 
-    def _exportar_xlsx(self):
-        with ExcelWriter(
-                path=os.path.join(self.path, 'Database.xlsx'),
-                engine='xlsxwriter',
-                date_format='DD/MM/YYYY'
-        ) as self.writer:
-            self._planilhar(nome='Base Ativa', situação='Cursando')
-            self._planilhar(nome='Base Bruta')
-            self._planilhar(nome='Transferidos', situação='(transferido)')
+    def __init__(self, consulta: DataFrame, path: Path | PathLike) -> None:
+        self._consulta = consulta
 
+        self._gerar_xlsx(path)
 
-    def _planilhar(self, nome: str, situação: Literal['Cursando', '(transferido)'] | None = None):
-        df = DataFrame()
-
-        if situação is None:
-            df: DataFrame = self.consulta
-            df.to_excel(self.writer, sheet_name=nome)
-
-        elif situação == 'Cursando':
-            df: DataFrame = self.consulta[self.consulta['Situação'] == 'Cursando']
-            df.to_excel(self.writer, sheet_name=nome)
-
-        elif situação == '(transferido)':
-            df: DataFrame = self.consulta[self.consulta['Situação'] == '(transferido)']
-            df.to_excel(self.writer, sheet_name=nome)
+    def _gerar_xlsx(self, path: Path) -> None:
+        _path = Path(path, f'{self.NOME_XLSX}.xlsx')
+        with ExcelWriter(path=_path, engine='xlsxwriter',  date_format='DD/MM/YYYY') as writer:
+            self._gerar_planilha(writer, self._df_ativa, 'Base Ativa')
+            self._gerar_planilha(writer, self._df_bruta, 'Base Bruta')
+            self._gerar_planilha(writer, self._df_transferidos, 'Transferidos')
 
 
-        self._formatar_planilha(df=df, nome_planilha=nome)
+    def _gerar_planilha(self, writer: ExcelWriter, df: DataFrame, nome_planilha: str):
+        df.to_excel(writer, sheet_name=nome_planilha)
+        pasta_de_trabalho = writer.book
+        planilha = writer.sheets[nome_planilha]
+        formato_fonte = self._formatos(pasta_de_trabalho)['fonte']
+        sem_borda = self._formatos(pasta_de_trabalho)['borderless']
+        planilha.set_column(0, df.shape[1], None, formato_fonte)
+        planilha.set_row(0, None, sem_borda)
 
-    def _formatar_planilha(self, df, nome_planilha):
-        # TODO: Organizar e modular essa merda aqui
+        self._gerar_tabela(df, pasta_de_trabalho, planilha, nome_planilha)
 
-        fonte_dict = {
-            "font_name": "Times New Roman",
-            "font_size": 12
-        }
-        formato_tabela = {
-            'name': nome_planilha.replace(' ', '_'),
-            'columns': [{'header': 'Índice'}] + [{'header': col} for col in self.consulta.columns],
+
+    def _gerar_tabela(self, df, pasta, planilha, nome) -> None:
+        formato = self._formatos(pasta)
+        linhas = df.shape[0]
+        colunas = df.shape[1] + 1
+        planilha.add_table(0, 0, linhas, colunas - 1, self._formato_tabela(nome))
+        planilha.hide_gridlines(2)
+
+        for linha in range(1, linhas, + 1):
+            planilha.write(linha, 0, df.index[linha - 1], formato)
+
+
+    def _formatos(self, pasta_) -> dict[Literal['fonte', 'borderless'], Any]:
+        formato_fonte = pasta_.add_format(self._fontes)
+        formato_sem_borda = pasta_.add_format({'border': 0})
+        return {'fonte' : formato_fonte, 'borderless' : formato_sem_borda}
+
+    def _formato_tabela(self, nome):
+        return {
+            'name': nome.replace(' ', '_'),
+            'columns': [{'header': 'Índice'}] + [{'header': col} for col in self._consulta.columns],
             'style': 'Table Style Medium 2',
             'autofilter': True
         }
 
-        pasta_de_trabalho = self.writer.book
-        planilha = self.writer.sheets[nome_planilha]
 
-        fonte_geral = pasta_de_trabalho.add_format(fonte_dict)
-        sem_borda = pasta_de_trabalho.add_format({'border': 0})
+    @property
+    def _fontes(self):
+        return {
+            "font_name": "Times New Roman",
+            "font_size": 12
+        }
 
-        planilha.set_column(0, df.shape[1], None, fonte_geral)
-        planilha.set_row(0, None, sem_borda)
+    # def _localizar_coluna(self, coluna: str):
+    #     i = self._consulta.columns.get_loc(coluna) + 1
+    #     return i
 
-        linhas = df.shape[0]
-        colunas = df.shape[1] + 1
-        planilha.add_table(0, 0, linhas, colunas - 1, formato_tabela)
-        planilha.hide_gridlines(2)
+    @property
+    def _df_ativa(self) -> DataFrame :
+        df = DataFrame()
+        df: DataFrame = self._consulta[self._consulta['Situação'] == 'Cursando']
+        return df
 
-        for linha in range(1, linhas, + 1):
-            planilha.write(linha, 0, df.index[linha - 1], sem_borda)
+    @property
+    def _df_bruta(self) -> DataFrame :
+        df: DataFrame = DataFrame(self._consulta)
+        return df
 
-    def _localizar_coluna(self, coluna: str):
-        i = self.consulta.columns.get_loc(coluna) + 1
-        return i
+    @property
+    def _df_transferidos(self) -> DataFrame :
+        df = DataFrame()
+        df: DataFrame = self._consulta[self._consulta['Situação'] == '(transferido)']
+        return df
