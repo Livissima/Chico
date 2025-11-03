e7ec5b5b51c0ee59ad572b664b5c44b4ee9055c3
Livissima <livianevesgeo@gmail.com>
2025-11-03 14:30:52 -0300

    Imports cancelados para evitar erro de importação circular.

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,4 +1,4 @@
-from .env_config import ID_SIGE, SENHA_SIGE, __IDS_SIAP, __SENHAS_SIAP, __USUÁRIOS_SIAP
+# from .env_config import ID_SIGE, SENHA_SIGE, __IDS_SIAP, __SENHAS_SIAP, __USUÁRIOS_SIAP
 
 
 
