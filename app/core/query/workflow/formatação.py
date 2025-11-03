19511b12579995161e5938350a6e8b8d5197db03
Livissima <livianevesgeo@gmail.com>
2025-11-03 14:33:50 -0300

    Método obsoleto `renomear colunas` removido. Atributos e variáveis públicas passadas para privado.

--- app/core/query/workflow/formatação.py
+++ app/core/query/workflow/formatação.py
@@ -6,25 +6,25 @@ from pandas import DataFrame
 class Formatação:
     #todo adicionar método para reordenar as colunas
 
-    colunas_datas: list[str] = [
+    _colunas_datas: list[str] = [
         'Data de Nascimento', 'Data Matrícula', 'Data Situação'
     ]
-    colunas_title: list[str] = [
+    _colunas_title: list[str] = [
         'Estudante', 'Nome Responsável', 'Filiação 1', 'Filiação 2', 'Município de Naturalidade', 'Nacionalidade',
         'Endereço: Logradouro', 'Endereço: Bairro', 'Endereço: Município', 'Irmão 1'
     ]
-    colunas_num: list[str] = [
+    _colunas_num: list[str] = [
         'Certidão de Nascimento: Folha', 'Certidão de Nascimento: Livro', 'Certidão de Nascimento: Termo'
     ]
-    colunas_num_string: list[str] = [
+    _colunas_num_string: list[str] = [
         'Matrícula', 'CPF Aluno', 'Filiação 1 - CPF', 'Filiação 2 - CPF', 'INEP'
     ]
 
     def __init__(self, df_integrado: DataFrame):
         print(f'\nFormatação instanciada.\n')
-        self.df = df_integrado
+        self._df = df_integrado
 
-        print(f'Chegou como: {self.df.shape}')
+        print(f'Chegou como: {self._df.shape}')
         self.df_formatado: DataFrame = self._formatar(df_integrado)
         print(f'Resultado: {self.df_formatado.shape}\n')
 
@@ -44,14 +44,14 @@ class Formatação:
 
     def _title(self, df):
         dataframe = df
-        for coluna in self.colunas_title:
+        for coluna in self._colunas_title:
             dataframe[coluna] = dataframe[coluna].astype(str).str.title()
             continue
         return dataframe
 
     def _numerizar(self, df) :
         dataframe = df.copy()
-        for coluna in self.colunas_num :
+        for coluna in self._colunas_num :
             extracted = dataframe[coluna].astype(str).str.extract(r'(\d+)')[0]
             numeric_series = pd.to_numeric(extracted, errors='coerce')
             numeric_series = numeric_series.replace([np.inf, -np.inf], np.nan)
@@ -63,7 +63,7 @@ class Formatação:
 
     def _datar(self, df):
         dataframe = df
-        for coluna in self.colunas_datas:
+        for coluna in self._colunas_datas:
             dataframe[coluna] = pd.to_datetime(dataframe[coluna], format = '%d/%m/%Y')
             dataframe[coluna] = dataframe[coluna].dt.strftime('%d/%m/%Y')
             continue
@@ -71,8 +71,8 @@ class Formatação:
 
     def _stringzar_números(self, df):
         dataframe = df
-        dataframe[self.colunas_num_string] = dataframe[self.colunas_num_string].astype(str)
-        for coluna in self.colunas_num_string:
+        dataframe[self._colunas_num_string] = dataframe[self._colunas_num_string].astype(str)
+        for coluna in self._colunas_num_string:
             dataframe[coluna] = dataframe[coluna].str.replace(
                 '.', '', regex = False).str.replace(
                 '-', '', regex = False).str.replace(
@@ -91,10 +91,6 @@ class Formatação:
         df.columns = [str(coluna).replace('\r', ' ').replace('\n', ' ') for coluna in df.columns]
         return df
 
-    @staticmethod
-    def renomear_colunas(df: DataFrame, dicionário: dict[str, str]) -> DataFrame:
-        df.rename(columns = dicionário, inplace = True)
-        return df
 
     def __getattr__(self, item):
         return getattr(self.df_formatado, item)
