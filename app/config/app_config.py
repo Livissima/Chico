082836a574171ddfe2abbf31ce3511f440f8b8d5
Livissima <livianevesgeo@gmail.com>
2025-11-04 18:17:55 -0300

    Construindo funcionalidades auxiliares para o processo de registro e organização de faltas.

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -1,16 +1,17 @@
 import re
 from pathlib import Path
 from platformdirs import user_documents_dir
+import json
+import os
 
 
 
-DIRETÓRIO_BASE_PADRÃO = Path(user_documents_dir(), 'SIGE')
-
+# DIRETÓRIO_BASE_PADRÃO = Path(user_documents_dir(), 'SIGE')
+#todo: Retornar o diretório base para a pasta de documentos, que é mais global. Para isso, preciso resolver a questão
+# do armazenamento de parâmetros da sessão. Coloquei no OneDrive para facilitar os testes com as planilhas de frequência.
+DIRETÓRIO_BASE_PADRÃO = Path(os.getenv('OneDriveCommercial'), 'SIGE')
 
 def obter_string_numérica(número) -> str :
     if not número :
         return '-'
-
-    string_numérica = re.sub(r'\D', '', str(número))
-
-    return string_numérica
+    return re.sub(r'\D', '', str(número))
