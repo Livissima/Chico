f8e8b9ee7bf3596e5f515ea6717a9e660adbc8cd
Livissima <livianevesgeo@gmail.com>
2025-11-11 18:25:42 -0300

    Nova task provisória em construção: Uniformizar

---
+++ tests/uni.py
@@ -0,0 +1,5 @@
+from app.auto import Bot
+
+DF_UNIFORMES = r'C:\Users\meren\OneDrive - Secretaria de Estado da Educação\Controle de tamanhos de uniformes.xlsx'
+
+Bot(tarefa='uniformizar', path=DF_UNIFORMES)
