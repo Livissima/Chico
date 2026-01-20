121768f394cce10ce3143512704249c4a1b0a992
Livissima <128195954+Livissima@users.noreply.github.com>
2026-01-20 02:32:53 -0300

    Adicionada constante de diretório da pasta secretaria

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -10,6 +10,7 @@ import os
 #todo: Retornar o diretório base para a pasta de documentos, que é mais global. Para isso, preciso resolver a questão
 # do armazenamento de parâmetros da sessão. Coloquei no OneDrive para facilitar os testes com as planilhas de frequência.
 DIRETÓRIO_BASE_PADRÃO = Path(os.getenv('OneDriveCommercial'), 'SIGE')
+DIRETÓRIO_SECRETARIA = Path(os.getenv('OneDriveCommercial'), 'Secretaria')
 
 def obter_string_numérica(número: str) -> str :
     if not número :
