23a0d1733b2b7b74ddd340f10d3207ca399b4b68
Livissima <128195954+Livissima@users.noreply.github.com>
2026-01-20 02:35:28 -0300

    Função criar pasta de docs dos estudantes.

--- tests/pastas_alunos.py
+++ tests/pastas_alunos.py
@@ -1,17 +1,27 @@
+#TODO: Realocar este módulo para um pacote apropriado.
 import os
+from pathlib import Path
+
 import pandas as pd
 
-path_pasta_geral = r'C:\Users\meren\OneDrive - Secretaria de Estado da Educação\Secretaria\Estudantes'
-path_database = r'C:\Users\meren\OneDrive - Secretaria de Estado da Educação\SIGE\Database.xlsx'
-path_cancelados = r'C:\Users\meren\OneDrive - Secretaria de Estado da Educação\Secretaria\Matrículas canceladas 2026.xlsx'
+from app.config.app_config import DIRETÓRIO_BASE_PADRÃO, DIRETÓRIO_SECRETARIA
+
+user = 'livia'
+path_pasta_estudantes = Path(DIRETÓRIO_SECRETARIA, 'Estudantes')
+path_database = Path(DIRETÓRIO_BASE_PADRÃO, 'Database.xksx')
+path_cancelados = Path(DIRETÓRIO_SECRETARIA, 'Matrículas canceladas 2026.xlsx')
 
 df_cancelados = pd.read_excel(path_cancelados)
 df_cancelados['Matrícula'] = df_cancelados['Matrícula'].replace(['\xa0', '-'], ['', ''], regex=True)
 
-matrícula = df_cancelados['Matrícula']
-estudante = df_cancelados['Nome']
-série = df_cancelados['Série']
+for índice, linha in df_cancelados.iterrows():
+    matrícula = linha['Matrícula']
+    estudante = linha['Nome']
+    série = linha['Série']
 
-dicio = dict(zip(matrícula, estudante, série))
-for i in dicio:
-    print(f'{i = }')
+    # nome_pasta = f'{série}º ~ {estudante}  - {matrícula}'
+    nome_pasta = f'{estudante}   ~   {matrícula}'
+    pasta = Path(path_pasta_estudantes, nome_pasta)
+    print(pasta)
+    if not pasta.exists():
+        pasta.mkdir(parents=True, exist_ok=True)
