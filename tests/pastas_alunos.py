21c7759b0e782fe270914f57765fd47f68213cce
Livissima <livianevesgeo@gmail.com>
2026-01-21 18:59:26 -0300

    ajuste no criador de pastas

--- tests/pastas_alunos.py
+++ tests/pastas_alunos.py
@@ -3,25 +3,31 @@ import os
 from pathlib import Path
 
 import pandas as pd
+from pandas import DataFrame
 
 from app.config.app_config import DIRETÓRIO_BASE_PADRÃO, DIRETÓRIO_SECRETARIA
 
-user = 'livia'
 path_pasta_estudantes = Path(DIRETÓRIO_SECRETARIA, 'Estudantes')
-path_database = Path(DIRETÓRIO_BASE_PADRÃO, 'Database.xksx')
+path_database = Path(DIRETÓRIO_BASE_PADRÃO, 'Database.xlsx')
 path_cancelados = Path(DIRETÓRIO_SECRETARIA, 'Matrículas canceladas 2026.xlsx')
 
 df_cancelados = pd.read_excel(path_cancelados)
 df_cancelados['Matrícula'] = df_cancelados['Matrícula'].replace(['\xa0', '-'], ['', ''], regex=True)
 
-for índice, linha in df_cancelados.iterrows():
-    matrícula = linha['Matrícula']
-    estudante = linha['Nome']
-    série = linha['Série']
+df_database = pd.read_excel(path_database, sheet_name='Base Ativa')
 
-    # nome_pasta = f'{série}º ~ {estudante}  - {matrícula}'
-    nome_pasta = f'{estudante}   ~   {matrícula}'
-    pasta = Path(path_pasta_estudantes, nome_pasta)
-    print(pasta)
-    if not pasta.exists():
-        pasta.mkdir(parents=True, exist_ok=True)
+
+def criar_pastas(df: DataFrame):
+    for índice, linha in df.iterrows():
+        matrícula = linha['Matrícula']
+        estudante = linha['Estudante']
+        série = linha['Série']
+
+        nome_pasta = f'{estudante}   ~   {matrícula}'
+        pasta = Path(path_pasta_estudantes, nome_pasta)
+        print(pasta)
+        if not pasta.exists():
+            pasta.mkdir(parents=True, exist_ok=True)
+
+
+criar_pastas(df_database)
