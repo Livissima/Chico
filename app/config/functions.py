44870b999a7af579e713ed0189329c5c815eb8e0
Livissima <livianevesgeo@gmail.com>
2026-01-29 18:43:04 -0300

    ajuste no criador de pastas

--- app/config/functions.py
+++ app/config/functions.py
@@ -1,7 +1,17 @@
 import re
 
 
+
+
 def obter_string_numérica(número: str) -> str :
     if not número :
         return '-'
     return re.sub(r'\D', '', str(número))
+
+def normalizar_diacrítica(texto) -> str:
+    import unicodedata
+
+    return ''.join(
+        c for c in unicodedata.normalize('NFKD', texto)
+        if not unicodedata.combining(c)
+    )
