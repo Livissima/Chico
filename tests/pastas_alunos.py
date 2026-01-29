5fa69d2c786bf55f26483710cb72f1e950d65424
Livissima <livianevesgeo@gmail.com>
2026-01-29 18:43:04 -0300

    ajuste no criador de pastas

--- tests/pastas_alunos.py
+++ tests/pastas_alunos.py
@@ -1,13 +1,16 @@
 #TODO: Realocar este módulo para um pacote apropriado.
-import os
+
+#TODO: organizar a lógica para usar as pastas de letras iniciais, em vez de jogar tudo junto
 from pathlib import Path
 
 import pandas as pd
 from pandas import DataFrame
 
 from app.config.app_config import DIRETÓRIO_BASE_PADRÃO, DIRETÓRIO_SECRETARIA
+from app.config.functions import normalizar_diacrítica
 
 path_pasta_estudantes = Path(DIRETÓRIO_SECRETARIA, 'Estudantes')
+
 path_database = Path(DIRETÓRIO_BASE_PADRÃO, 'Database.xlsx')
 path_cancelados = Path(DIRETÓRIO_SECRETARIA, 'Matrículas canceladas 2026.xlsx')
 
@@ -16,21 +19,34 @@ df_cancelados['Matrícula'] = df_cancelados['Matrícula'].replace(['\xa0', '-'],
 
 df_database = pd.read_excel(path_database, sheet_name='Base Ativa')
 
+def inicial_da_pasta(nome_pasta: str) -> str:
+    return normalizar_diacrítica(nome_pasta).strip()[0].upper()
+
 
 def criar_pastas(df: DataFrame):
-
     for índice, linha in df.iterrows():
         matrícula = linha['Matrícula']
         estudante = linha['Estudante']
-        série = linha['Série']
 
-        nome_pasta = f'{estudante}   ~   {matrícula}'
-        pasta = Path(path_pasta_estudantes, nome_pasta)
+        # NÃO MEXA NESSA LÓGICA
+        nome_pasta_estudante = f'{estudante}   ~   {matrícula}'
 
-        #NÃO MEXA NESSA LÓGICA
-        if not pasta.exists():
-            print(pasta)
-            pasta.mkdir(parents=True, exist_ok=True)
+        inicial = inicial_da_pasta(nome_pasta_estudante)
+        if not inicial:
+            continue
+
+        path_pasta_inicial = path_pasta_estudantes / inicial
+        if not path_pasta_inicial.exists():
+            print(f'Pasta de inicial inexistente: {inicial}')
+            continue
+
+        path_final = path_pasta_inicial / nome_pasta_estudante
+
+        try:
+            path_final.mkdir()
+            print(f'Criada: {path_final}')
+        except FileExistsError:
+            pass
 
 
 criar_pastas(df_database)
