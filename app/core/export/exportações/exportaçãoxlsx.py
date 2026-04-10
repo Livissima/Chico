4b1d59ab7dfb759f16849575a815a1004c7418b3
Livissima <livianevesgeo@gmail.com>
2026-04-10 13:10:27 -0300

    Leve melhoria na estrutura da ExportaçãoXLSX

--- app/core/export/exportações/exportaçãoxlsx.py
+++ app/core/export/exportações/exportaçãoxlsx.py
@@ -10,67 +10,68 @@ from app.core import ConsultaEstudantes
 class ExportaçãoXLSX :
     NOME_XLSX = 'Database'
 
-    def __init__(self, consulta: ConsultaEstudantes, path: Path | PathLike) -> None :
-        self._consulta = consulta
-        self._gerar_xlsx(path)
+    def __init__(self, consulta: ConsultaEstudantes, path: Path) -> None :
+        self._executar(consulta, path)
 
-    def _gerar_xlsx(self, path: Path) -> None :
+    def _executar(self, consulta, path):
         _path = Path(path, f'{self.NOME_XLSX}.xlsx')
-        with ExcelWriter(path=_path, engine='xlsxwriter', date_format='DD/MM/YYYY') as writer :
-            self._gerar_planilha(writer, self._df_ativa, 'Base Ativa')
-            self._gerar_planilha(writer, self._df_bruta, 'Base Bruta')
-            self._gerar_planilha(writer, self._df_transferidos, 'Transferidos')
 
-    def _gerar_planilha(self, writer: ExcelWriter, df: DataFrame, nome_planilha: str) :
-        df.to_excel(writer, sheet_name=nome_planilha, index=False)
+        df_ativa = consulta[consulta['Situação'] == 'Cursando'].copy()
+        df_bruta = consulta.copy()
+        df_transferidos = consulta[consulta['Situação'] == '(transferido)'].copy()
+
+        self._gerar_xlsx(_path, [df_ativa, df_bruta, df_transferidos])
+
+
+    def _gerar_xlsx(self, path: Path, dfs: list[DataFrame]) -> None :
+        with ExcelWriter(path=path, engine='xlsxwriter', date_format='DD/MM/YYYY') as writer :
+            self._planilhar_dataframe(writer, dfs[0], 'Base Ativa')
+            self._planilhar_dataframe(writer, dfs[1], 'Base Bruta')
+            self._planilhar_dataframe(writer, dfs[2], 'Transferidos')
+
+
+    def _planilhar_dataframe(self, writer: ExcelWriter, dataframe: DataFrame, nome_planilha: str) :
+        dataframe.to_excel(writer, sheet_name=nome_planilha, index=False)
         pasta_de_trabalho = writer.book
         planilha = writer.sheets[nome_planilha]
 
-        formatos = self._formatos(pasta_de_trabalho)
-        formato_fonte = formatos['fonte']
-        formato_cabeçalho = formatos['cabeçalho']
+        formato_fonte = self.__formatar_fonte(pasta_de_trabalho)
+        formato_cabeçalho = self.__formatar_cabeçalho(pasta_de_trabalho)
 
-        planilha.set_column(0, df.shape[1] - 1, None, formato_fonte)
+        planilha.set_column(0, dataframe.shape[1] - 1, None, formato_fonte)
 
-        for col_num, valor in enumerate(df.columns.values) :
+        for col_num, valor in enumerate(dataframe.columns.values) :
             planilha.write(0, col_num, valor, formato_cabeçalho)
 
-        self._gerar_tabela(df, pasta_de_trabalho, planilha, nome_planilha)
+        self.__formatar_como_tabela(dataframe, planilha, nome_planilha)
 
     @staticmethod
-    def _gerar_tabela(df, pasta, planilha, nome) -> None :
+    def __formatar_como_tabela(df, planilha, nome) -> None :
         linhas = df.shape[0]
         colunas = df.shape[1]
 
         # Configurar a tabela
         planilha.add_table(0, 0, linhas, colunas - 1, {
-            'name' : nome.replace(' ', '_'), 'columns' : [{'header' : col} for col in df.columns],
-            'style' : 'Table Style Medium 2', 'autofilter' : True
+            'name' : nome.replace(' ', '_'),
+            'columns' : [{'header' : col} for col in df.columns],
+            'style' : 'Table Style Medium 2',
+            'autofilter' : True
         })
         planilha.hide_gridlines(2)
 
     @staticmethod
-    def _formatos(pasta_) -> dict[str, Any] :
-        formato_fonte = pasta_.add_format({
-            "font_name" : "Times New Roman", "font_size" : 12
+    def __formatar_cabeçalho(pasta_de_trabalho) -> dict[str, str | int | bool] :
+        return pasta_de_trabalho.add_format({
+            "font_name" : "Times New Roman",
+            "font_size" : 12,
+            "bold" : True,
+            "bg_color" : "#D3D3D3",
+            "border" : 1
         })
 
-        formato_cabeçalho = pasta_.add_format({
-            "font_name" : "Times New Roman", "font_size" : 12, "bold" : True, "bg_color" : "#D3D3D3", "border" : 1
+    @staticmethod
+    def __formatar_fonte(pasta_de_trabalho) -> dict[str, str | int] :
+        return pasta_de_trabalho.add_format({
+            "font_name" : "Times New Roman",
+            "font_size" : 12
         })
-
-        return {
-            'fonte' : formato_fonte, 'cabeçalho' : formato_cabeçalho
-        }
-
-    @property
-    def _df_ativa(self) -> DataFrame :
-        return self._consulta[self._consulta['Situação'] == 'Cursando'].copy()
-
-    @property
-    def _df_bruta(self) -> DataFrame :
-        return self._consulta.copy()
-
-    @property
-    def _df_transferidos(self) -> DataFrame :
-        return self._consulta[self._consulta['Situação'] == '(transferido)'].copy()
