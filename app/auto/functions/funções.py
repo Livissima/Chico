b3fe5edee2280ade56b2f30abb3e7213616fdf52
Livissima <livianevesgeo@gmail.com>
2026-04-15 18:32:44 -0300

    Nova gunção global para facilitar testes isolados.

--- app/auto/functions/funções.py
+++ app/auto/functions/funções.py
@@ -1,3 +1,6 @@
+import warnings
+from openpyxl import __name__ as openpyxl_name
+import pandas as pd
 import unicodedata
 
 def normalizar_unicode(texto: str) -> str:
@@ -12,3 +15,8 @@ def normalizar_dicionário(dicionário: dict | None):
         return {}
     return {normalizar_unicode(chave): valor for chave, valor in dicionário.items()}
 
+def ajustar_print_pandas():
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.expand_frame_repr', False)  # Evita quebra do DataFrame em múltiplas linhas
+    pd.set_option('display.width', None)  # Ajusta automaticamente à largura do terminal
+    warnings.filterwarnings('ignore', category=UserWarning, module=openpyxl_name)
